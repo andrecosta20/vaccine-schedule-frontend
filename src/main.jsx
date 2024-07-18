d400fc282607fbb7f1195318c4847f5af6fcfb09
andrecosta20 <costento.inscritos@gmail.com>
2024-07-18 18:49:24 -0300

    Inicializa ponto de entrada principal com roteamento

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainApp from './MainApp';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
