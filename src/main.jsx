25f7b01f67a5a45b13d34db749adc4e54a87bc75
andrecosta20 <costento.inscritos@gmail.com>
2024-07-19 18:12:02 -0300

    refactor: remove arquivos antigos após reorganização

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainApp from './MainApp';
-import './index.css';
+import MainApp from './components/MainApp';
+import './assets/images/styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
